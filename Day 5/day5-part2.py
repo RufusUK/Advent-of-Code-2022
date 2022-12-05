1d6ac9d0d1c766f2a80999cf30a71b6c9418fd39
RufusUK <ruth@otn.co.uk>
2022-12-05 21:12:57 +0000

    Day 5 Complete

---
+++ Day 5/day5-part2.py
@@ -0,0 +1,39 @@
+# Set up variables
+Crates = {}
+toplist = ''
+
+# Import libraries
+
+# Open File
+file = open("Day 5\input.txt", "r")
+input = file.read()
+
+# Create a List of the file entries
+list = input.split("\n")
+
+# Close the file
+file.close()
+
+# Find location of blank line
+blank = list.index('')
+
+#Initialise the crates - work upwards from the blank line to populate the initial position
+for i in range(len(list[blank-1].split())):
+    Crates[i+1] = []
+    for ele in range(blank-2,-1,-1):
+        if list[ele][1 + (i * 4)] != ' ':
+            Crates[i+1].append(list[ele][1 + (i * 4)])
+
+#Now undertake the moves
+for ele in range(blank+1,len(list)):
+    instruction=list[ele].split()
+    Crates[int(instruction[5])] = Crates[int(instruction[5])] + Crates[int(instruction[3])][len(Crates[int(instruction[3])])-int(instruction[1]):]
+    Crates[int(instruction[3])]=Crates[int(instruction[3])][:len(Crates[int(instruction[3])])-int(instruction[1])]
+
+# Now we need the top crate from each stack, which will be the last entry in the list
+
+for ele in Crates:
+    toplist = toplist + Crates[ele][-1]
+
+print(toplist)
+    
